fa4280787c449591c2268d4c0d83db59cc2ab657
Jhern603 <Jhern603@fiu.edu>
2020-12-22 10:29:15 -0500

    Enhanced Accesibility
    Deployment to firebase (v0.1.0)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="navbar">
-      <Link to="/" className="navbar_title">
+      <Link to="/" className="navbar_title" tabIndex="0">
         Jose Hernandez
       </Link>
 
       <ul className="navbar_links">
         <li className="navbar_item">
-          <Link to="/about" className="navbar_text">
+          <Link to="/about" className="navbar_text" tabIndex="0">
             About
           </Link>
         </li>
 
         <li className="navbar_item">
-          <Link to="/contact" className="navbar_text">
+          <Link to="/contact" className="navbar_text" tabIndex="0">
             Contact
           </Link>
         </li>
 
         <li className="navbar_item">
-          <Link to="/login" className="login">
+          <Link to="/login" className="login" tabIndex="0">
             Login
           </Link>
         </li>
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: "AIzaSyBdeDJYlrz-iEMb8wpVRihXESjoWhYg3M8",
+  authDomain: "testproject-22be1.firebaseapp.com",
+  databaseURL: "https://testproject-22be1.firebaseio.com",
+  projectId: "testproject-22be1",
+  storageBucket: "testproject-22be1.appspot.com",
+  messagingSenderId: "386269924537",
+  appId: "1:386269924537:web:df4ff36090e46d312d1f68",
+};
+firebase.initializeApp(firebaseConfig);
+export default firebaseConfig;
